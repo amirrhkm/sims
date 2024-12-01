5fb1c912255967c16069851d2970f4e9352be09f
amirrhkm <amir.zaid@silentmode.my>
2024-12-01 23:00:32 +0800

    feat(routes): clean up web routes

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
 
 Route::get('/', function () {
     return view('welcome')->name('welcome');
 });
 
-// Inventori route for "Pengurus"
+// ---------------------------------- Route for "Pengurus" ----------------------------------
 Route::get('/pengurus/inventori', function () {
     return view('pengurus.inventori');
 });
 
-// Route for "Pemohon"
+// ---------------------------------- Route for "Pemohon" ----------------------------------
 Route::get('/pemohon/dashboard', function () {
     return view('pemohon.dashboard');
 });
 
 Route::get('/pemohon/inventori', function () {
-    return view('pemohon.inventori');
+    return view('pemohon.inventori-main');
 });
 
 Route::get('/pemohon/inventori/borang-permohonan', function () {
-    return view('pemohon.borang-permohonan');
+    return view('pemohon.inventori-borang-permohonan');
 });
 
 Route::get('/pemohon/inventori/lihat-permohonan', function () {
-    return view('pemohon.lihat-permohonan');
+    return view('pemohon.inventori-lihat-permohonan');
 });
 
-Route::get('/pemohon/inventori/lihat-inventori', function () {
-    return view('pemohon.lihat-inventori');
-});
+Route::get('/pemohon/inventori/lihat-inventori', [InventoryController::class, 'index'])
+    ->name('pemohon.inventori.lihat');
