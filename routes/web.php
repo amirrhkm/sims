9c6ca40d2f87144be8c89c22832d39b43933b0d6
amirrhkm <amir.zaid@silentmode.my>
2024-12-03 22:04:16 +0800

    feat(ruotes): endpoints for pengurus inventory actions

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,45 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventoryController;
 
-Route::get('/', function () {
-    return view('welcome')->name('welcome');
-});
+// Route::get('/', function () {
+//     return view('login')->name('login');
+// });
 
 // ---------------------------------- Route for "Pengurus" ----------------------------------
+Route::get('/pengurus/dashboard', function () {
+    return view('pengurus.dashboard');
+});
+
 Route::get('/pengurus/inventori', function () {
-    return view('pengurus.inventori');
+    return view('pengurus.inventori-main');
+});
+
+Route::get('/pengurus/inventori/kemaskini-inventori',
+    [InventoryController::class, 'kemaskini'])
+    ->name('pengurus.inventori-kemaskini');
+
+Route::get('/pengurus/inventori/kemaskini-inventori/item/add', 
+    [InventoryController::class, 'create'])
+    ->name('pengurus.inventori-kemaskini-item-add');
+
+Route::post('/pengurus/inventori/kemaskini-inventori/item/add',
+    [InventoryController::class, 'store'])
+    ->name('pengurus.inventori-kemaskini-item-add');
+
+Route::get('/pengurus/inventori/kemaskini-inventori/item/edit/{id}',
+    [InventoryController::class, 'edit'])
+    ->name('pengurus.inventori-kemaskini-item-edit');
+
+Route::put('/pengurus/inventori/kemaskini-inventori/item/edit/{id}',
+    [InventoryController::class, 'update'])
+    ->name('pengurus.inventori-kemaskini-item-edit');
+
+Route::delete('/pengurus/inventori/kemaskini-inventori/item/hapus/{id}',
+    [InventoryController::class, 'destroy'])
+    ->name('pengurus.inventori-kemaskini-item-hapus');
+
+Route::get('/pengurus/inventori/semak-permohonan', function () {
+    return view('pengurus.inventori-semak-permohonan');
 });
 
 // ---------------------------------- Route for "Pemohon" ----------------------------------
