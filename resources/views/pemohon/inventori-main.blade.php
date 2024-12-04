4897c44657d5475e0da4e14cc3e507f2685b7243
amirrhkm <amir.zaid@silentmode.my>
2024-12-04 16:27:50 +0800

    fix(inventory): redirect to the correct routes

--- resources/views/pemohon/inventori-main.blade.php
+++ resources/views/pemohon/inventori-main.blade.php
@@ -12,7 +12,7 @@
                 <h1 class="text-2xl font-semibold text-gray-900">Inventori</h1>
                 
                 <div class="navigation-buttons flex gap-4 mt-6 w-1/2">
-                    <a href="/pemohon/inventori/borang-permohonan" 
+                    <a href="{{ route('pemohon.inventori-borang-permohonan') }}" 
                         class="flex-1 bg-gray-800 text-white text-center font-semibold py-4 px-1 rounded-xl shadow-lg hover:bg-gray-900 hover:shadow-xl hover:-translate-y-0.5 transition-all duration-200 ease-out">
                         Permohonan Baru
                     </a>
