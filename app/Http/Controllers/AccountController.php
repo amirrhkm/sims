a358af01fdc51295086864d20221e58db658487e
amirrhkm <amir.zaid@silentmode.my>
2024-12-16 23:36:18 +0800

    fix(user-management): handle logics for user management form

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
@@ -22,8 +23,7 @@ class AccountController extends Controller
 
     public function create()
     {
-        $users = User::all();
-        return view('pengurus.pengguna-add', compact('users'));
+        return view('pengurus.pengguna-add');
     }
 
     public function store(Request $request)
@@ -54,8 +54,40 @@ class AccountController extends Controller
     public function update(Request $request, $id)
     {
         $user = User::findOrFail($id);
-        $user->update($request->all());
-        return redirect()->route('pengurus.pengguna')->with('success', 'Pengguna berjaya diubah.');
+        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:8',
+            'role' => 'required|in:pemohon,pengurus',
+            'position' => 'required|string|max:255',
+            'grade' => 'required|string|max:255',
+            'department' => 'required|string|max:255',
+            'section' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+        ]);
+
+        $userData = $request->only([
+            'name',
+            'email',
+            'role',
+            'position',
+            'grade',
+            'department',
+            'section',
+            'phone_number'
+        ]);
+
+        if ($request->filled('password')) {
+            $userData['password'] = Hash::make($request->password);
+        } else {
+            $userData['password'] = $user->password;
+        }
+
+        $user->update($userData);
+
+        return redirect()->route('pengurus.pengguna-edit', $id)
+            ->with('success', 'Pengguna berjaya diubah.');
     }
 
     public function destroy($id)
@@ -63,5 +95,11 @@ class AccountController extends Controller
         $user = User::findOrFail($id);
         $user->delete();
         return redirect()->route('pengurus.pengguna')->with('success', 'Pengguna berjaya dihapuskan.');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('pemohon.pengguna-show', compact('user'));
     }
 }
